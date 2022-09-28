dfd64c77248573e4911e05d832e3db1c5170cfe7
ben waldee <b1.waldee@gmail.com>
2022-09-27 19:32:34 -0500

    form styling

--- react-app/src/components/auth/LoginForm.js
+++ react-app/src/components/auth/LoginForm.js
@@ -2,9 +2,10 @@ import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 import { login } from '../../store/session';
+import '../CSS/LoginForm.css'
 
 const LoginForm = () => {
-  const [errors, setErrors] = useState([]);
+  const [errors, setErrors] = useState("");
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const user = useSelector(state => state.session.user);
@@ -31,34 +32,40 @@ const LoginForm = () => {
   }
 
   return (
-    <form onSubmit={onLogin}>
-      <div>
-        {errors.map((error, ind) => (
-          <div key={ind}>{error}</div>
-        ))}
-      </div>
-      <div>
-        <label htmlFor='email'>Email</label>
-        <input
-          name='email'
-          type='text'
-          placeholder='Email'
-          value={email}
-          onChange={updateEmail}
-        />
-      </div>
-      <div>
-        <label htmlFor='password'>Password</label>
-        <input
-          name='password'
-          type='password'
-          placeholder='Password'
-          value={password}
-          onChange={updatePassword}
-        />
-        <button type='submit'>Login</button>
-      </div>
-    </form>
+    <div className='LoginForm_formWrap'>
+      <form className='LoginForm_form' onSubmit={onLogin}>
+        <h1 className='LoginForm_title'>Log In</h1>
+        <div className='LoginForm_subtitle'>Already have an account? Log in below.</div>
+        {errors && <div className='LoginForm_error'>Incorrect login information</div>}
+        <div className='LoginForm_grid'>
+          <div className='LoginForm_inputWrap'>
+            <label className='LoginForm_label' htmlFor='email'>Email</label>
+            <input
+              className='LoginForm_input'
+              name='email'
+              type='text'
+              placeholder='Email'
+              value={email}
+              onChange={updateEmail}
+            // required={true}
+            />
+          </div>
+          <div className='LoginForm_inputWrap'>
+            <label className='LoginForm_label' htmlFor='password'>Password</label>
+            <input
+              className='LoginForm_input'
+              name='password'
+              type='password'
+              placeholder='Password'
+              value={password}
+              onChange={updatePassword}
+            // required={true}
+            />
+          </div>
+          <button type='submit'>Login</button>
+        </div>
+      </form>
+    </div>
   );
 };
 
--- react-app/src/components/auth/SignUpForm.js
+++ react-app/src/components/auth/SignUpForm.js
@@ -1,11 +1,17 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux'
 import { Redirect } from 'react-router-dom';
 import { signUp } from '../../store/session';
+import '../CSS/SignUpForm.css'
 
 const SignUpForm = () => {
-  const [errors, setErrors] = useState([]);
-  const [username, setUsername] = useState('');
+  const [errors, setErrors] = useState({
+    email: "",
+    username: "",
+    password: "",
+    repeatPassword: ""
+  });
+  const [username, setUsername] = useState("");
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [repeatPassword, setRepeatPassword] = useState('');
@@ -14,13 +20,45 @@ const SignUpForm = () => {
 
   const onSignUp = async (e) => {
     e.preventDefault();
+
+    let newErr = {}
+
+    if (username.length < 3 || username.length > 30) {
+      setUsername("")
+      newErr.username = "Username must be 3-30 characters"
+    }
+    if (!email.includes("@") || !email.includes(".")) {
+      setEmail("")
+      newErr.email = "Email in use or invalid."
+
+    }
+    if (password !== repeatPassword) {
+      setRepeatPassword("")
+      newErr.repeatPassword = "Passwords must match"
+    }
+
+    // lengths
+    if (username.length < 1) newErr.username = "Please enter a username."
+    if (email.length < 1) newErr.email = "Please enter an email."
+    if (password.length < 1) newErr.password = "Please enter a password."
+    if (repeatPassword.length < 1) newErr.repeatPassword = "Please confirm your password."
+
+    setErrors(newErr)
+
+    if (Object.values(newErr).length > 0) {
+      newErr = {}
+      return
+    }
+
     if (password === repeatPassword) {
       const data = await dispatch(signUp(username, email, password));
       if (data) {
-        setErrors(data)
+        setErrors({ email: "Email in use or invalid." })
       }
+
     }
   };
+
 
   const updateUsername = (e) => {
     setUsername(e.target.value);
@@ -38,56 +76,85 @@ const SignUpForm = () => {
     setRepeatPassword(e.target.value);
   };
 
+
   if (user) {
     return <Redirect to='/' />;
   }
 
+
   return (
-    <form onSubmit={onSignUp}>
-      <div>
-        {errors.map((error, ind) => (
-          <div key={ind}>{error}</div>
-        ))}
+    <form className='SignUpForm_form' onSubmit={onSignUp}>
+      <h1 className='SignUpForm_title'>Sign Up for a Free Account</h1>
+      <div className='SignUpForm_subtitle'>Please join Crunchyscroll! It's only one click away!</div>
+      <div className='SignUpForm_innerFormGrid'>
+        <div className='SignUpForm_inputWrap'>
+          <label className='SignUpForm_label'>User Name</label>
+          <input
+            className='SignUpForm_input'
+            type='text'
+            name='username'
+            onChange={(e) => {
+
+              updateUsername(e)
+              return
+            }}
+            value={username}
+          // required={true}
+          ></input>
+          {errors?.username && <div className='SignUpForm_error'>{errors.username}</div>}
+        </div>
+        <div className='SignUpForm_inputWrap'>
+          <label className='SignUpForm_label'>Email</label>
+          <input
+            className='SignUpForm_input'
+            type='text'
+            name='email'
+            onChange={(e) => {
+
+              updateEmail(e)
+              return
+            }}
+            value={email}
+          // required={true}
+          ></input>
+          {errors?.email && <div className='SignUpForm_error'>{errors.email}</div>}
+        </div>
+        <div className='SignUpForm_inputWrap'>
+          <label className='SignUpForm_label'>Password</label>
+          <input
+            className='SignUpForm_input'
+            type='password'
+            name='password'
+            onChange={(e) => {
+
+              updatePassword(e)
+              return
+            }}
+            value={password}
+          // required={true}
+          ></input>
+          {errors?.password && <div className='SignUpForm_error'>{errors.password}</div>}
+        </div>
+        <div className='SignUpForm_inputWrap'>
+          <label className='SignUpForm_label'>Confirm Password</label>
+          <input
+            className='SignUpForm_input'
+            type='password'
+            name='repeat_password'
+            onChange={(e) => {
+
+              updateRepeatPassword(e)
+              return
+            }}
+            value={repeatPassword}
+          // required={true}
+          ></input>
+          {errors?.repeatPassword && <div className='SignUpForm_error'>{errors.repeatPassword}</div>}
+        </div>
+        <button className='SignUpForm_submit' type='submit'>Create Account</button>
       </div>
-      <div>
-        <label>User Name</label>
-        <input
-          type='text'
-          name='username'
-          onChange={updateUsername}
-          value={username}
-        ></input>
-      </div>
-      <div>
-        <label>Email</label>
-        <input
-          type='text'
-          name='email'
-          onChange={updateEmail}
-          value={email}
-        ></input>
-      </div>
-      <div>
-        <label>Password</label>
-        <input
-          type='password'
-          name='password'
-          onChange={updatePassword}
-          value={password}
-        ></input>
-      </div>
-      <div>
-        <label>Confirm Password</label>
-        <input
-          type='password'
-          name='repeat_password'
-          onChange={updateRepeatPassword}
-          value={repeatPassword}
-          required={true}
-        ></input>
-      </div>
-      <button type='submit'>Sign Up</button>
-    </form>
+    </form >
+
   );
 };
 
