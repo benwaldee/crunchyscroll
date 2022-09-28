18bbc1b83c52a63db57097232492a18f4195008e
ben waldee <b1.waldee@gmail.com>
2022-09-28 11:58:58 -0500

    footer done - make sure wrapper of each page has bottom padding = footer

--- react-app/src/components/Footer.js
+++ react-app/src/components/Footer.js
@@ -9,15 +9,20 @@ const Footer = () => {
         <footer className='Footer_outer'>
             <div className='Footer_grid'>
                 <div className='Footer_contentWrap'>
-                    <div>Connect With Me</div>
-                    <a>Github</a>
-                    <a>LinkedIn</a>
+                    <div className='Footer_contentHead'>Connect With Me</div>
+                    <a href='https://github.com/benwaldee' target='_blank' className='Footer_content'>Github</a>
+                    <a href='https://www.linkedin.com/in/benwaldee/' target='_blank' className='Footer_content'>LinkedIn</a>
                 </div>
                 <div className='Footer_contentWrap'>
-                    <div>Crunchyscroll</div>
-                    <a>Codebase</a>
-                    <a>Wireframes</a>
-                    <a>ReadME</a>
+                    <div className='Footer_contentHead'>Crunchyscroll</div>
+                    <a href='https://github.com/benwaldee/crunchyscroll' target='_blank' className='Footer_content'>Codebase</a>
+                    <a href='https://github.com/benwaldee/crunchyscroll/wiki/Wireframes' target='_blank' className='Footer_content'>Wireframes</a>
+                    <a href='https://github.com/benwaldee/crunchyscroll/blob/main/README.md' target='_blank' className='Footer_content'>ReadME</a>
+                </div>
+                <div className='Footer_contentWrap'>
+                    <div className='Footer_contentHead'>Other Projects</div>
+                    <a href='https://airbnbeep.herokuapp.com/' target='_blank' className='Footer_content'>AirBnBeep</a>
+                    <a href='https://aa-bad-reads.herokuapp.com/' target='_blank' className='Footer_content'>Badreads</a>
                 </div>
             </div>
         </footer>
