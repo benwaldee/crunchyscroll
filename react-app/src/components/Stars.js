0544d521c42f0de67d649b7fee0bf3045eae5072
ben waldee <b1.waldee@gmail.com>
2022-09-30 15:17:47 -0500

    stars and reviews subhead done

---
+++ react-app/src/components/Stars.js
@@ -0,0 +1,52 @@
+import "./CSS/Stars.css"
+import React, { useEffect } from 'react';
+import starEmpty from './images/stars/star-empty.png'
+import starQuart from './images/stars/star-quart.png'
+import starHalf from './images/stars/star-half.png'
+import starThQuart from './images/stars/star-three-quart.png'
+import starFull from './images/stars/star-full.png'
+
+
+const Stars = ({ avgRating, reviews }) => {
+
+    console.log(avgRating)
+    console.log(reviews)
+    if (Object.values(reviews).length === 0) { return <div className="Stars_noRatings"> No Ratings yet</div> }
+    return (
+        <>
+            {avgRating < 1 && avgRating < 0.25 && <img className="Stars_empty1 Stars_star" src={starEmpty}></img>}
+            {avgRating < 1 && avgRating < 0.5 && avgRating >= 0.25 && <img className="Stars_quarter1 Stars_star" src={starQuart}></img>}
+            {avgRating < 1 && avgRating < 0.75 && avgRating >= 0.5 && <img className="Stars_half1 Stars_star" src={starHalf}></img>}
+            {avgRating < 1 && avgRating < 1 && avgRating >= 0.75 && <img className="Stars_3quarter1 Stars_star" src={starThQuart}></img>}
+            {avgRating >= 1 && <img className="Stars_full1 Stars_star" src={starFull}></img>}
+
+            {avgRating < 2 && avgRating < 1.25 && <img className="Stars_empty2 Stars_star" src={starEmpty}></img>}
+            {avgRating < 2 && avgRating < 1.5 && avgRating >= 1.25 && <img className="Stars_quarter2 Stars_star" src={starQuart}></img>}
+            {avgRating < 2 && avgRating < 1.75 && avgRating >= 1.5 && <img className="Stars_half2 Stars_star" src={starHalf}></img>}
+            {avgRating < 2 && avgRating < 2 && avgRating >= 1.75 && <img className="Stars_3quarter2 Stars_star" src={starThQuart}></img>}
+            {avgRating >= 2 && <img className="Stars_full2 Stars_star" src={starFull}></img>}
+
+            {avgRating < 3 && avgRating < 2.25 && <img className="Stars_empty3 Stars_star" src={starEmpty}></img>}
+            {avgRating < 3 && avgRating < 2.5 && avgRating >= 2.25 && <img className="Stars_quarter3 Stars_star" src={starQuart}></img>}
+            {avgRating < 3 && avgRating < 2.75 && avgRating >= 2.5 && <img className="Stars_half3 Stars_star" src={starHalf}></img>}
+            {avgRating < 3 && avgRating < 3 && avgRating >= 2.75 && <img className="Stars_3quarter3 Stars_star" src={starThQuart}></img>}
+            {avgRating >= 3 && <img className="Stars_full3 Stars_star" src={starFull}></img>}
+
+            {avgRating < 4 && avgRating < 3.25 && <img className="Stars_empty4 Stars_star" src={starEmpty}></img>}
+            {avgRating < 4 && avgRating < 3.5 && avgRating >= 3.25 && <img className="Stars_quarter4 Stars_star" src={starQuart}></img>}
+            {avgRating < 4 && avgRating < 3.75 && avgRating >= 3.5 && <img className="Stars_half4 Stars_star" src={starHalf}></img>}
+            {avgRating < 4 && avgRating < 4 && avgRating >= 3.75 && <img className="Stars_3quarter4 Stars_star" src={starThQuart}></img>}
+            {avgRating >= 4 && <img className="Stars_full4 Stars_star" src={starFull}></img>}
+
+            {avgRating < 5 && avgRating < 4.25 && <img className="Stars_empty1 Stars_star" src={starEmpty}></img>}
+            {avgRating < 5 && avgRating < 4.5 && avgRating >= 4.25 && <img className="Stars_quarter1 Stars_star" src={starQuart}></img>}
+            {avgRating < 5 && avgRating < 4.75 && avgRating >= 4.5 && <img className="Stars_half1 Stars_star" src={starHalf}></img>}
+            {avgRating < 5 && avgRating < 4 && avgRating >= 4.75 && <img className="Stars_3quarter1 Stars_star" src={starThQuart}></img>}
+            {avgRating >= 5 && <img className="Stars_full1 Stars_star" src={starFull}></img>}
+
+
+        </>
+    )
+}
+
+export default Stars;
--- react-app/src/components/StoryByID.js
+++ react-app/src/components/StoryByID.js
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import React, { useEffect } from 'react';
 import { getAllStoriesThunk } from '../store/stories'
 import { getStoryReviewsThunk } from '../store/reviews.js'
+import Stars from './Stars'
 
 const StoryByID = () => {
     const history = useHistory()
@@ -11,6 +12,16 @@ const StoryByID = () => {
     let { id } = useParams();
     const user = useSelector(state => state.session.user)
     const story = useSelector(state => state?.stories?.allStories[Number(id)])
+    const reviews = useSelector(state => state?.reviews?.storyReviews)
+    //turn reviews into arr, map to just arr of stars, reduce to avg of stars
+    const avgRating = Object.values(useSelector(state => state?.reviews?.storyReviews))
+        .map(review => review?.stars)
+        .reduce((acc, star, index, array) => {
+            return acc + star / array.length
+        }, 0)
+        .toFixed(2)
+
+    console.log("avgRating", avgRating)
 
     useEffect(() => {
         dispatch(getAllStoriesThunk())
@@ -21,19 +32,27 @@ const StoryByID = () => {
     return (
         <div className='StoryByID_contentWrap'>
             <div className='StoryByID_header'>
-                <img className='StoryByID_backgroundImage' src={story?.image_url}></img>
-                <img className='StoryByID_mainImage' src={story?.image_url}></img>
+                <img className='StoryByID_backgroundImage' src={story?.image_url}
+                    onError={e => { e.currentTarget.src = "http://media.comicbook.com/2018/03/zwru5zwigvntizfbv54x-1088958.jpeg"; }}></img>
+                <img className='StoryByID_mainImage' src={story?.image_url}
+                    onError={e => { e.currentTarget.src = "http://media.comicbook.com/2018/03/zwru5zwigvntizfbv54x-1088958.jpeg"; }}
+                ></img>
             </div>
             <div className='StoryByID_subHeader'>
                 <div className='StoryByID_subHeaderLeft'>
                     <h1 className='StoryByID_title'>{story?.title}</h1>
                     <div className='StoryByID_ratingWrap'>
-                        <div className='StoryByID_stars'></div>
-                        <div className='StoryByID_rating'></div>
-                        <div className='StoryByID_reviews'></div>
+                        <div className='StoryByID_stars'>
+                            <Stars avgRating={avgRating} reviews={reviews} />
+                        </div >
+                        <div className='StoryByID_avgRatingWrap'>
+                            <div className='StoryByID_rating'>Average Rating:</div>
+                            <div className='StoryByID_numRating'>{avgRating}</div>
+                        </div>
+                        <div className='StoryByID_reviews'>{Object.values(reviews).length} reviews</div>
                     </div>
                     <div className='StoryByID_listWrap'>
-                        <div className='StoryByID_watchlist'></div>
+                        <div className='StoryByID_watchlist'> </div>
                         <div className='StoryByID_crunchylist'></div>
                     </div>
                 </div>
